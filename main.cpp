25ff1d35741153738b86c2969b77b8e2b54d1ccd
Adham Khaled <adhamkhaledm7@gmail.com>
2019-07-16 15:18:34 +0200

    Added standard deviation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-
+#include <cmath>
+#include <iomanip>
 using std::cout;
 using std::endl;
 using std::vector;
@@ -11,12 +12,12 @@ int main(){
     char selection {};
     do{
         cout<<"\nP - Print numbers"<<"\nA - Add a number"<<"\nM - Display mean of the numbers";
-        cout<<"\nC - Clear the list"<<"\nF - Search for an item in the list";
+        cout<<"\nC - Clear the list"<<"\nF - Search for an item in the list"<<"\nD - Display the standard deviation of the integers";
         cout<<"\nS - Display the smallest number"<<"\nL - Display the largest number"<<"\nQ - Quit"<<endl;
         cout<<"Enter your choice: ";
         cin>>selection;
         switch (selection){
-            case 'p':
+            case 'p': //Print all the integers
             case 'P':
                 if (list.size()==0)
                     cout<<"[] - the list is empty"<<endl;
@@ -36,7 +37,25 @@ int main(){
                 cout<<add<<" added!";
                 break;
             }
-            case 'm':
+            case 'd': // Standard deviation
+            case 'D':
+                if (list.size() == 0)
+                    cout<<"Unable to calculate the standard deviation - no data";
+                else{
+                    int sum {};
+                    for (auto element: list)
+                        sum += element;
+                    double mean {static_cast<double>(sum)/list.size()};
+                    double sumdist {};
+                    for (auto element: list){
+                        sumdist += pow(abs(element-mean),2);
+                        }
+                    double sd {sqrt(sumdist/list.size())};
+                    
+                    cout<<"The standard deviation of the integers is "<<std::setprecision(3)<<sd<<endl;
+                }
+                break;
+            case 'm': // Mean of the integers
             case 'M':
                 if (list.size() == 0)
                     cout<<"Unable to calculate the mean - no data";
@@ -47,7 +66,7 @@ int main(){
                     cout<<"The mean of your list is "<<static_cast<double>(sum)/list.size()<<endl;
                 }
                 break;
-            case 's':
+            case 's': //Smallest integer
             case 'S':
                 if (list.size()==0)
                     cout<<"Unable to determine the smallest number - list is empty"<<endl;
@@ -60,7 +79,7 @@ int main(){
                         }
                     cout<<"The smallest number in the list is "<<min<<endl;
                 }break;
-            case 'l':
+            case 'l': //Largest integer
             case 'L':
                 if (list.size()==0)
                     cout<<"Unable to determine the largest number - list is empty"<<endl;
@@ -72,12 +91,12 @@ int main(){
                         }
                     cout<<"The largest number in the list is "<<max<<endl;
                 }break;
-            case 'c':
+            case 'c': //Clear the list
             case 'C':
             	list.clear();
             	cout<<"The list has been cleared"<<endl;
             	break;
-            case 'f':
+            case 'f': //Find how many times is the integer found in the list
             case 'F':{
             	int search {};
             	unsigned count {};
@@ -95,7 +114,7 @@ int main(){
             		cout<<"The integer: "<<search<<" was found "<<count<<" times in the list."<<endl;
             	break;
             }
-            case 'q':
+            case 'q': //Quit the program
             case 'Q':
                 cout<<"Goodbye!";
                 break;
